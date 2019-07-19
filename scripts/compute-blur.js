f5a8c854aa35f7e4a695bf00d0deaaf077d8c5dd
Justin Fan <justin_fan@apple.com>
2019-07-19 13:38:53 -0700

    Use same module for both shaders

--- scripts/compute-blur.js
+++ scripts/compute-blur.js
@@ -1,6 +1,4 @@
 const threadsPerThreadgroup = 32;
-const blurRadius = 4;
-const byteMask = (1 << 8) - 1;
 
 const originalBufferBindingNum = 0;
 const outputBufferBindingNum = 1;
@@ -14,42 +12,6 @@ const weights = [
     0.0162162162,
 ];
 const weightsBufferSize = weights.length * Float32Array.BYTES_PER_ELEMENT;
-
-const shaderUtils = `
-uint getR(uint rgba)
-{
-    return rgba & ${byteMask};
-}
-
-uint getG(uint rgba)
-{
-    return (rgba >> 8) & ${byteMask};
-}
-
-uint getB(uint rgba)
-{
-    return (rgba >> 16) & ${byteMask};
-}
-
-uint getA(uint rgba)
-{
-    return (rgba >> 24) & ${byteMask};
-}
-
-uint makeRGBA(uint r, uint g, uint b, uint a)
-{
-    // Because little-endian? ¯\_(ツ)_/¯
-    return r + (g << 8) + (b << 16) + (a << 24);
-}
-`;
-
-const blurWeightsArray = `float[5] weights;
-weights[0] = 0.2270270270;
-weights[1] = 0.1945945946;
-weights[2] = 0.1216216216;
-weights[3] = 0.0540540541;
-weights[4] = 0.0162162162;
-`;
 
 async function computeBlur() {
     if (!navigator.gpu) {
@@ -139,93 +101,26 @@ async function computeBlur() {
     const pipelineLayout = device.createPipelineLayout({ bindGroupLayouts: [bindGroupLayout] });
 
     // Set up pipelines
-    const horizontalModule = device.createShaderModule({ 
-        code: shaderUtils + `
-[numthreads(${threadsPerThreadgroup}, 1, 1)]
-compute void horizontal(constant uint[] origBuffer : register(u${originalBufferBindingNum}),
-                        device uint[] outputBuffer : register(u${outputBufferBindingNum}),
-                        constant float[] weights : register(b${weightsBufferBindingNum}),
-                        float3 dispatchThreadID : SV_DispatchThreadID)
-{
-    uint globalIndex = uint(dispatchThreadID.y) * ${image.width} + uint(dispatchThreadID.x);
-
-    uint r = 0;
-    uint g = 0;
-    uint b = 0;
-    uint a = 0;
-
-    for (int i = -${blurRadius}; i <= ${blurRadius}; ++i) {
-        uint startColor = origBuffer[uint(int(globalIndex) + i)];
-        float weight = weights[uint(abs(i))];
-        r += uint(float(getR(startColor)) * weight);
-        g += uint(float(getG(startColor)) * weight);
-        b += uint(float(getB(startColor)) * weight);
-        a += uint(float(getA(startColor)) * weight);
-    }
-
-    outputBuffer[globalIndex] = makeRGBA(r, g, b, a);
-}`, 
-        isWHLSL: true
-    });
+    const shaderModule = device.createShaderModule({ code: createShaderCode(image), isWHLSL: true });
 
     device.pushErrorScope("validation");
 
     const horizontalPipeline = device.createComputePipeline({ 
         layout: pipelineLayout, 
         computeStage: {
-            module: horizontalModule,
+            module: shaderModule,
             entryPoint: "horizontal"
         }
     });
 
     device.popErrorScope().then(error => { if (error) console.log(error.message); });
 
-    const verticalModule = device.createShaderModule({
-        code: shaderUtils + `
-uint verticallyOffsetIndex(uint index, int offset)
-{
-    int realOffset = offset * ${image.width};
-
-    if (int(index) + realOffset < 0)
-        return 0;
-    
-    return uint(int(index) + realOffset);
-}
-
-[numthreads(1, ${threadsPerThreadgroup}, 1)]
-compute void vertical(device uint[] origBuffer : register(u${originalBufferBindingNum}),
-                        constant uint[] middleBuffer : register(u${outputBufferBindingNum}),
-                        constant float[] weights : register(b${weightsBufferBindingNum}),
-                        float3 dispatchThreadID : SV_DispatchThreadID)
-{
-    uint globalIndex = uint(dispatchThreadID.x) * ${image.height} + uint(dispatchThreadID.y);
-
-    uint r = 0;
-    uint g = 0;
-    uint b = 0;
-    uint a = 0;
-
-    for (int i = -${blurRadius}; i <= ${blurRadius}; ++i) {
-        uint startColor = middleBuffer[verticallyOffsetIndex(globalIndex, i)];
-        float weight = weights[uint(abs(i))];
-        r += uint(float(getR(startColor)) * weight);
-        g += uint(float(getG(startColor)) * weight);
-        b += uint(float(getB(startColor)) * weight);
-        a += uint(float(getA(startColor)) * weight);
-    }
-
-    origBuffer[globalIndex] = makeRGBA(r, g, b, a);
-}
-`,
-        isWHLSL: true
-    });
-
     device.pushErrorScope("validation");
 
     const verticalPipeline = device.createComputePipeline({
         layout: pipelineLayout,
         computeStage: {
-            module: verticalModule,
+            module: shaderModule,
             entryPoint: "vertical"
         }
     });
@@ -257,3 +152,98 @@ compute void vertical(device uint[] origBuffer : register(u${originalBufferBindi
 }
 
 window.addEventListener("load", computeBlur);
+
+/* Shaders */
+
+const blurRadius = 4;
+const byteMask = (1 << 8) - 1;
+
+function createShaderCode(image) {
+    return `
+uint getR(uint rgba)
+{
+    return rgba & ${byteMask};
+}
+
+uint getG(uint rgba)
+{
+    return (rgba >> 8) & ${byteMask};
+}
+
+uint getB(uint rgba)
+{
+    return (rgba >> 16) & ${byteMask};
+}
+
+uint getA(uint rgba)
+{
+    return (rgba >> 24) & ${byteMask};
+}
+
+uint makeRGBA(uint r, uint g, uint b, uint a)
+{
+    // Because little-endian? ¯\_(ツ)_/¯
+    return r + (g << 8) + (b << 16) + (a << 24);
+}
+
+uint verticallyOffsetIndex(uint index, int offset)
+{
+    int realOffset = offset * ${image.width};
+
+    if (int(index) + realOffset < 0)
+        return 0;
+    
+    return uint(int(index) + realOffset);
+}
+
+[numthreads(${threadsPerThreadgroup}, 1, 1)]
+compute void horizontal(constant uint[] origBuffer : register(u${originalBufferBindingNum}),
+                        device uint[] outputBuffer : register(u${outputBufferBindingNum}),
+                        constant float[] weights : register(b${weightsBufferBindingNum}),
+                        float3 dispatchThreadID : SV_DispatchThreadID)
+{
+    uint globalIndex = uint(dispatchThreadID.y) * ${image.width} + uint(dispatchThreadID.x);
+
+    uint r = 0;
+    uint g = 0;
+    uint b = 0;
+    uint a = 0;
+
+    for (int i = -${blurRadius}; i <= ${blurRadius}; ++i) {
+        uint startColor = origBuffer[uint(int(globalIndex) + i)];
+        float weight = weights[uint(abs(i))];
+        r += uint(float(getR(startColor)) * weight);
+        g += uint(float(getG(startColor)) * weight);
+        b += uint(float(getB(startColor)) * weight);
+        a += uint(float(getA(startColor)) * weight);
+    }
+
+    outputBuffer[globalIndex] = makeRGBA(r, g, b, a);
+}
+
+[numthreads(1, ${threadsPerThreadgroup}, 1)]
+compute void vertical(device uint[] origBuffer : register(u${originalBufferBindingNum}),
+                        constant uint[] middleBuffer : register(u${outputBufferBindingNum}),
+                        constant float[] weights : register(b${weightsBufferBindingNum}),
+                        float3 dispatchThreadID : SV_DispatchThreadID)
+{
+    uint globalIndex = uint(dispatchThreadID.x) * ${image.height} + uint(dispatchThreadID.y);
+
+    uint r = 0;
+    uint g = 0;
+    uint b = 0;
+    uint a = 0;
+
+    for (int i = -${blurRadius}; i <= ${blurRadius}; ++i) {
+        uint startColor = middleBuffer[verticallyOffsetIndex(globalIndex, i)];
+        float weight = weights[uint(abs(i))];
+        r += uint(float(getR(startColor)) * weight);
+        g += uint(float(getG(startColor)) * weight);
+        b += uint(float(getB(startColor)) * weight);
+        a += uint(float(getA(startColor)) * weight);
+    }
+
+    origBuffer[globalIndex] = makeRGBA(r, g, b, a);
+}
+`;
+}
